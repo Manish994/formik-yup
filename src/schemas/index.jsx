7237394f39c66a36b06766b84730dccd01bb12a2
Manish Pokhrel <61821715+Manish994@users.noreply.github.com>
2022-09-16 12:55:30 +0545

    Update index.jsx

--- src/schemas/index.jsx
+++ src/schemas/index.jsx
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 export const signUpYup = Yup.object({
   name: Yup.string().min(2).max(25).required("Please Enter Your name"),
-  email: Yup.string().email().required("Please Enter your email"),
+  email: Yup.string().email("Please enter valid email").required("Please Enter your email"),
   password: Yup.string().min(6).required("Please Eneter your password"),
   confirmpassword: Yup.string()
     .required()
